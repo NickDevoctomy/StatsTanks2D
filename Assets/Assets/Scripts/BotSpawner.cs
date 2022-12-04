cafe9410f186c87e4e530ae61f4021f1ca00d492
Nick Pateman <nick@npsoftware.co.uk>
2022-12-04 15:43:45 +0000

    Now spawning a single bot to test with.  Need to work on seeded bot logic.

--- Assets/Assets/Scripts/BotSpawner.cs
+++ Assets/Assets/Scripts/BotSpawner.cs
@@ -10,6 +10,10 @@ public class BotSpawner : MonoBehaviour
     void Start()
     {
         _spawnPoints = GameObject.FindGameObjectsWithTag("BotSpawnPoint");
+        var bot = GameObject.Instantiate(BotPrefab, transform);
+        // Pick a random spawn point here, for now just pick 0 as we only have 1 for testing
+        var randomSpawnPointIndex = 0;
+        bot.transform.position = _spawnPoints[randomSpawnPointIndex].transform.position;
     }
 
     void Update()
