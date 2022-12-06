97ff87cec5619cb256fffab13b38d99d8902f51d
Nick Pateman <nick@npsoftware.co.uk>
2022-12-06 20:33:10 +0000

    Slight tweaking to gizmo drawing and put in logic that can determine when destination has been reached.

--- Assets/Assets/Scripts/Bot.cs
+++ Assets/Assets/Scripts/Bot.cs
@@ -1,29 +1,40 @@
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UIElements;
-using static UnityEngine.GraphicsBuffer;
 
 public class Bot : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    private Vector3 _lastPosition;
 
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    //void Update()
-    //{
-    //}
+    void Update()
+    {
+        if (!_navMeshAgent.pathPending &&
+            Vector3.Distance(transform.position, _lastPosition) < 0.5f)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f)
+                {
+                    _lastPosition = transform.position;
+                }
+            }
+        }
+    }
 
     void OnDrawGizmos()
     {
-        if(_navMeshAgent?.path != null)
+        if (_navMeshAgent?.path != null)
         {
-            Gizmos.color = Color.red;
             for (int i = 0; i < _navMeshAgent.path.corners.Length - 1; i++)
             {
+                Gizmos.color = Vector3.Distance(transform.position, _navMeshAgent.path.corners[i + 1]) > 5f ? Color.red : Color.green;
                 Gizmos.DrawSphere(_navMeshAgent.path.corners[i + 1], 0.5f);
+                Gizmos.color = Color.red;
                 Gizmos.DrawLine(
                     _navMeshAgent.path.corners[i],
                     _navMeshAgent.path.corners[i + 1]);
@@ -34,7 +45,6 @@ public class Bot : MonoBehaviour
 
     public void CalculatePath(Vector3 targetPosition)
     {
-        var navMeshPath = new NavMeshPath();
         _navMeshAgent.SetDestination(targetPosition);
     }
 
@@ -52,6 +62,7 @@ public class Bot : MonoBehaviour
             }
 
             transform.position = closestHit.position;
+            _lastPosition = transform.position;
         }
     }
 }
