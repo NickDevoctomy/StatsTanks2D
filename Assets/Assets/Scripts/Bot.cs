0502874e72bb62dc34a95e995072512926fde2e6
Nick Pateman <nick@npsoftware.co.uk>
2022-12-10 16:23:27 +0000

    Fix issue of bot drifting along the ground when doing sharp turns.  Also added navmeshobject to player, bot will ow drive around the player.

--- Assets/Assets/Scripts/Bot.cs
+++ Assets/Assets/Scripts/Bot.cs
@@ -1,27 +1,60 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Bot : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
-    private Vector3 _lastPosition;
+    private Rigidbody _rigidBody;
+    private bool _forceSharpTurn;
+    private Vector3 _turnTarget;
+    private bool _isSharpTurning;
 
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (!_navMeshAgent.pathPending &&
-            Vector3.Distance(transform.position, _lastPosition) < 0.5f)
+        if (!_isSharpTurning &&
+            !_navMeshAgent.pathPending &&
+            _navMeshAgent.path.corners.Length > 1)
         {
-            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            var angleToNext = GetAngleToNext(null, out var nextCorner);
+            if(!_forceSharpTurn &&
+                Mathf.Abs(angleToNext) > 90)
             {
-                if (!_navMeshAgent.hasPath || _navMeshAgent.velocity.sqrMagnitude == 0f)
-                {
-                    _lastPosition = transform.position;
-                }
+                // Our angle to next position is too sharp, and we don't want to drift
+                // so let's stop navigation and perform a sharp turn
+                _forceSharpTurn = true;
+                _navMeshAgent.isStopped = true;
+                _turnTarget = nextCorner;
+                _rigidBody.velocity = Vector3.zero;
+                _rigidBody.angularVelocity = Vector3.zero;
+                _isSharpTurning = true;
+            }
+        }
+
+        if(_forceSharpTurn &&
+            _navMeshAgent.path.corners.Length > 1)
+        {
+            var angleToNext = GetAngleToNext(_turnTarget, out var nextCorner);
+            if (Mathf.Abs(angleToNext) > 5)
+            {
+                // Sharp turn toward next nav position
+                var targetRotation = Quaternion.LookRotation(_turnTarget - transform.position);
+                var str = Mathf.Min(2.0f * Time.deltaTime, 1);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, str);
+            }
+            else
+            {
+                // We finished our turn, let's resume navigation
+                _isSharpTurning = false;
+                _navMeshAgent.isStopped = false;
+                _forceSharpTurn = false;
             }
         }
     }
@@ -32,20 +65,45 @@ public class Bot : MonoBehaviour
         {
             for (int i = 0; i < _navMeshAgent.path.corners.Length - 1; i++)
             {
-                Gizmos.color = Vector3.Distance(transform.position, _navMeshAgent.path.corners[i + 1]) > 5f ? Color.red : Color.green;
-                Gizmos.DrawSphere(_navMeshAgent.path.corners[i + 1], 0.5f);
+                var curCorner = new Vector3(_navMeshAgent.path.corners[i].x, transform.position.y, _navMeshAgent.path.corners[i].z);
+                var nextCorner = new Vector3(_navMeshAgent.path.corners[i+1].x, transform.position.y, _navMeshAgent.path.corners[i+1].z);
+
+                Gizmos.color = Vector3.Distance(transform.position, nextCorner) > 5f ? Color.red : Color.green;
+                Gizmos.DrawSphere(nextCorner, 0.5f);
                 Gizmos.color = Color.red;
                 Gizmos.DrawLine(
-                    _navMeshAgent.path.corners[i],
-                    _navMeshAgent.path.corners[i + 1]);
+                    curCorner,
+                    nextCorner);
+            }
+
+            if (_forceSharpTurn)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(_turnTarget, 0.6f);
             }
         }
 
+        var forwardVector = transform.position + transform.forward * 2;
+        Gizmos.DrawLine(
+            transform.position,
+            forwardVector);
+    }
+
+    private float GetAngleToNext(Vector3? nextCorner, out Vector3 usedNextCorner)
+    {
+        var forwardVector = transform.position + transform.forward * 2;
+        usedNextCorner = nextCorner.HasValue ? nextCorner.GetValueOrDefault() : new Vector3(_navMeshAgent.path.corners[1].x, transform.position.y, _navMeshAgent.path.corners[1].z);
+        var side1 = forwardVector - transform.position;
+        var side2 = usedNextCorner - transform.position;
+        return Vector3.Angle(side1, side2);
     }
 
     public void CalculatePath(Vector3 targetPosition)
     {
+        _isSharpTurning = false;
+        _forceSharpTurn = false;
         _navMeshAgent.SetDestination(targetPosition);
+        _navMeshAgent.isStopped = false;
     }
 
     public void PositionOnNavMesh()
@@ -62,7 +120,6 @@ public class Bot : MonoBehaviour
             }
 
             transform.position = closestHit.position;
-            _lastPosition = transform.position;
         }
     }
 }
